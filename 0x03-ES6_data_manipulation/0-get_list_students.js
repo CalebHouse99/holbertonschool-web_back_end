92da01dbfb0510ab434a3ed445a9427f0034bbf7
Caleb House <calebhouse99@gmail.com>
2023-01-18 07:53:28 -0800

    order difference

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 export default function getListStudents() {
-  const firstStudent = { firstName: 'Guillaume',  id: 1, location: 'San Francisco' };
+  const firstStudent = { firstName: 'Guillaume', id: 1, location: 'San Francisco' };
   const secondStudent = { firstName: 'James', id: 2, location: 'Columbia' };
   const thirdStudent = { firstName: 'Serena', id: 5, location: 'San Francisco' };
   const arr = [firstStudent, secondStudent, thirdStudent];
