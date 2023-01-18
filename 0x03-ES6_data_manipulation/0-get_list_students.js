9733b1787fe41fa8886dfc5b9e416d67913c3977
Caleb House <calebhouse99@gmail.com>
2023-01-17 17:31:28 -0800

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,7 @@
-function getListStudents() {
+export default function getListStudents() {
   const firstStudent = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const secondStudent = { id: 2, firstName: 'James', location: 'Columbia' };
   const thirdStudent = { id: 5, firstName: 'Serena', location: 'San Francisco' };
   const arr = [firstStudent, secondStudent, thirdStudent];
   return arr;
 }
-
-console.log(getListStudents());
