e19974abebc685d357f62028a445ed58ae27a5a6
Caleb House <calebhouse99@gmail.com>
2023-01-18 19:26:32 -0800

    eslint

--- 0x03-ES6_data_manipulation/1-main.js
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -1,7 +1,7 @@
-import getListStudentIds from "./1-get_list_student_ids.js";
-import getListStudents from "./0-get_list_students.js";
+import getListStudentIds from './1-get_list_student_ids.js';
+import getListStudents from './0-get_list_students.js';
 
-console.log(getListStudentIds("hello"));
+console.log(getListStudentIds('hello'));
 console.log(getListStudentIds(getListStudents()));
 console.log(getListStudentIds(getListStudents()).length);
 console.log(getListStudentIds(getListStudents()));
