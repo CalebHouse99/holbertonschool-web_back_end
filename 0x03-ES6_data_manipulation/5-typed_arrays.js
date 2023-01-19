6d6d24fad4032c8f0d95d5dff15cd15d2274cc92
Caleb House <calebhouse99@gmail.com>
2023-01-18 19:07:17 -0800

    tasks 5 and 6

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,6 @@
+export default function createInt8TypedArray(length, position, value){
+  if (position > length) throw new Error('Position outside range');
+  const viewData = new DataView(new ArrayBuffer(length));
+  viewData.setInt8(position, value);
+  return viewData;
+}
