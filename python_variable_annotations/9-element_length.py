4a8f1c136f4b06d80356e36ef4a38d8013d053f4
Caleb House <calebhouse99@gmail.com>
2023-05-08 13:17:13 -0500

    missed list

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This function gets the length of the element"""
-from typing import Iterable, Sequence, Tuple
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Iterable[Sequence]) -> Tuple[Sequence, int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """using len, this gets the length of the element"""
     return [(i, len(i)) for i in lst]
