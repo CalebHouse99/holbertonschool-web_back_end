748cf76e35f28f2a6a628b9b788c04e6b1404e7c
Caleb House <calebhouse99@gmail.com>
2023-05-08 12:00:38 -0500

    documentation

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from typing import List, Union
-"""This acquires the summation of the list with,
-varying data types, such as ints and floats
+"""This passes an argument mxd_list,
+and returns the sum
 """
 
 
