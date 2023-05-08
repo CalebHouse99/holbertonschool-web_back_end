e5e70794d7e6d138a587b8981eed7373cb632706
Caleb House <calebhouse99@gmail.com>
2023-05-08 12:38:16 -0500

    pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """It's crucial that this comment comes before the imports"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes the string and numbers and returns a tuple of them"""
     return (k, v ** 2)
