a37f7da0bc0543379859079599484288199c271e
Caleb House <calebhouse99@gmail.com>
2023-05-08 12:10:36 -0500

    yet another documentation change attempt

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from typing import List, Union
 """This passes an argument mxd_list,
-returns the sum
+and returns the sum as a float
 """
 
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
-    """Returns the sum of the list, adding both ints and floats"""
+    """Returns the sum of both ints and floats"""
     return sum(mxd_list)
