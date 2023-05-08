60af5730188313fc0305302e4de88fa148743f19
Caleb House <calebhouse99@gmail.com>
2023-05-08 11:38:29 -0500

    had to import List from typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from typing import List
 """Sums the list and returns said sum"""
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Sum the list using sum()"""
     return sum(input_list)
