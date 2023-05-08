4b93c7633563cc14ad956625598d30ac922189a9
Caleb House <calebhouse99@gmail.com>
2023-05-08 11:46:56 -0500

    insane levels of documentation

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
+"""
+This module contains a single function, sum_list, that sums a list of floats.
+
+Functions:
+    sum_list(input_list: List[float]) -> float:
+        Given a list of floats, returns the sum of the list elements.
+"""
+
 from typing import List
-"""Sums the list and returns said sum of the input_list"""
 
 
 def sum_list(input_list: List[float]) -> float:
-    """Sum the list using sum() and List imported from typing"""
+    """
+    Given a list of floats, returns the sum of the list elements.
+
+    Args:
+        input_list (List[float]): A list of floats to be summed.
+
+    Returns:
+        float: The sum of the input list.
+
+    Example:
+        >>> sum_list([1.0, 2.0, 3.0])
+        6.0
+    """
     return sum(input_list)
