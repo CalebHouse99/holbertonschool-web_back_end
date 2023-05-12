4aba42865bfd207a18154d04c4eaea8097f1c248
unknown <calebhouse99@gmail.com>
2023-05-11 22:45:23 -0500

    pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,7 @@ from typing import List
 # Importing async_generator function
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Return 10 random numbers using an async comprehension"""
     return [i async for i in async_generator()]
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,7 @@ from typing import List
 # Importing async_comprehension function
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """Measure total runtime of async_comprehension four times in parallel"""
     start = time.time()
