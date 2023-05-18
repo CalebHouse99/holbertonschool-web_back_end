0ce1202b680222d6c9145ad9357794531ce27289
unknown <calebhouse99@gmail.com>
2023-05-17 23:35:59 -0500

    task 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -8,7 +8,8 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -22,15 +23,13 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {i: v for i, v in enumerate(self.dataset())}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
@@ -38,11 +37,11 @@ class Server:
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 
-        indexed_data = list(self.__indexed_dataset.items())
+        indexed_data = self.indexed_dataset()
         data, next_index = [], index
 
         while len(data) < page_size and next_index < len(indexed_data):
-            if indexed_data[next_index]:
+            if indexed_data.get(next_index):
                 data.append(indexed_data[next_index])
             next_index += 1
 
