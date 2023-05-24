f126ec991affbb6316633efdba984217cba14c9b
unknown <calebhouse99@gmail.com>
2023-05-24 16:56:08 -0500

    task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,4 +7,5 @@ import re
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Filters data and separates it"""
-    return re.sub(rf'({"|".join(fields)})=[^;]*', rf'\1={redaction}', message)
+    return re.sub(rf'({"|".join(fields)})=[^{separator}]*',
+                  rf'\1={redaction}', message)
