3613253e5794184364b4a94d1f9437df37fea3fd
unknown <calebhouse99@gmail.com>
2023-05-24 16:52:09 -0500

    forgot to return str

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,6 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
+                 separator: str) -> str:
     """Filters data and separates it"""
     return re.sub(rf'({"|".join(fields)})=[^;]*', rf'\1={redaction}', message)
