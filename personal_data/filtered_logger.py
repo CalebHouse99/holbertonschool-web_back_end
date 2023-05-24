3e8936e88df734b52ad421e6eb199e7897993f2f
unknown <calebhouse99@gmail.com>
2023-05-24 16:49:57 -0500

    task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is a crucial documentation"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str):
+    """Filters data and separates it"""
+    return re.sub(rf'({"|".join(fields)})=[^;]*', rf'\1={redaction}', message)
