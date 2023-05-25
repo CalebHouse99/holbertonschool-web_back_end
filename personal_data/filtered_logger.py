2f1e1e1785436419d7129731b75a0279bf341450
unknown <calebhouse99@gmail.com>
2023-05-24 23:39:35 -0500

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """This is a crucial documentation"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -9,3 +10,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """Filters data and separates it"""
     return re.sub(rf'({"|".join(fields)})=[^{separator}]*',
                   rf'\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_msg = record.getMessage()
+        redacted_msg = filter_datum(self.fields, self.REDACTION,
+                                    original_msg, self.SEPARATOR)
+        record.msg = redacted_msg
+        return super().format(record)
