46bd2d90c46884e83f64b95dec07fe606e27f228
unknown <calebhouse99@gmail.com>
2023-05-24 23:40:36 -0500

    documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -20,10 +20,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initialization"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Formatting function"""
         original_msg = record.getMessage()
         redacted_msg = filter_datum(self.fields, self.REDACTION,
                                     original_msg, self.SEPARATOR)
