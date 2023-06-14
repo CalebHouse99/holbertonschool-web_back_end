9fe83e520353d96c6f1d05c33960fe7f6307a723
Caleb House <calebhouse99@gmail.com>
2023-06-14 16:46:00 -0500

    task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
-        self.redis.flushdb()
-    
-    def store(data) -> str:
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         randKey = uuid.uuid4()
-        
+        self._redis.set(str(randKey), data)
+        return str(randKey)
