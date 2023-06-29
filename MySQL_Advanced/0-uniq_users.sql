85e104aaa4cb5ae7f3128fef8e538b7d655448dc
Caleb House <calebhouse99@gmail.com>
2023-06-29 14:01:52 -0500

    varchar instead of text

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --If the table exists, script doesn't fail
 --can be executed on any database
 CREATE TABLE IF NOT EXISTS users (
-    id int,
-    email text unique,
-    name text 
+    id INT NOT NULL,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    name VARCHAR(256)
 );
